253a67b4a0f9e80bfc87e06edf66414738e66d23
William Hames <melhoir@gmail.com>
2017-07-05 22:36:45 -0500

    scaffold product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629025010) do
+ActiveRecord::Schema.define(version: 20170706033349) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
